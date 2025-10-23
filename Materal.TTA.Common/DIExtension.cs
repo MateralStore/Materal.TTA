2687470f246a4562f55876ef8cfa78ee870651f9
陈明旭 <cloomcmx1554@hotmail.com>
2025-10-23 15:49:12 +0800

    Update:更新包版本

--- Materal.TTA.Common/DIExtension.cs
+++ Materal.TTA.Common/DIExtension.cs
@@ -1,42 +1,41 @@
 ﻿using NetCore.AutoRegisterDi;
 
-namespace Materal.TTA.Common
+namespace Materal.TTA.Common;
+
+/// <summary>
+/// DI扩展
+/// </summary>
+public static class DIExtension
 {
     /// <summary>
-    /// DI扩展
+    /// 添加TTA仓储服务
     /// </summary>
-    public static class DIExtension
+    /// <param name="services"></param>
+    /// <param name="repositoryAssemblies"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddTTARepository(this IServiceCollection services, params Assembly[] repositoryAssemblies)
     {
-        /// <summary>
-        /// 添加TTA仓储服务
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="repositoryAssemblies"></param>
-        /// <returns></returns>
-        public static IServiceCollection AddTTARepository(this IServiceCollection services, params Assembly[] repositoryAssemblies)
+        List<Assembly> repositoryAssemblyList = [.. repositoryAssemblies.Distinct()];
+        foreach (Assembly repositoryAssembly in repositoryAssemblyList)
         {
-            List<Assembly> repositoryAssemblyList = [.. repositoryAssemblies.Distinct()];
-            foreach (Assembly repositoryAssembly in repositoryAssemblyList)
-            {
-                services.RegisterAssemblyPublicNonGenericClasses(repositoryAssembly)
-                    .Where(m => (m.IsAssignableTo<IRepository>() || m.IsAssignableTo<IUnitOfWork>()) && !m.IsAbstract)
-                    .AsPublicImplementedInterfaces(ServiceLifetime.Scoped);
-            }
-            return services;
+            services.RegisterAssemblyPublicNonGenericClasses(repositoryAssembly)
+                .Where(m => (m.IsAssignableTo<IRepository>() || m.IsAssignableTo<IUnitOfWork>()) && !m.IsAbstract)
+                .AsPublicImplementedInterfaces(ServiceLifetime.Scoped);
         }
-        /// <summary>
-        /// 添加TTA仓储服务
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="services"></param>
-        /// <param name="repositoryAssemblies"></param>
-        /// <returns></returns>
-        public static IServiceCollection AddTTARepository<T>(this IServiceCollection services, params Assembly[] repositoryAssemblies)
-        {
-            List<Assembly> repositoryAssemblyList = [.. repositoryAssemblies];
-            repositoryAssemblyList.Add(typeof(T).Assembly);
-            services.AddTTARepository([.. repositoryAssemblyList]);
-            return services;
-        }
+        return services;
+    }
+    /// <summary>
+    /// 添加TTA仓储服务
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="services"></param>
+    /// <param name="repositoryAssemblies"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddTTARepository<T>(this IServiceCollection services, params Assembly[] repositoryAssemblies)
+    {
+        List<Assembly> repositoryAssemblyList = [.. repositoryAssemblies];
+        repositoryAssemblyList.Add(typeof(T).Assembly);
+        services.AddTTARepository([.. repositoryAssemblyList]);
+        return services;
     }
 }
