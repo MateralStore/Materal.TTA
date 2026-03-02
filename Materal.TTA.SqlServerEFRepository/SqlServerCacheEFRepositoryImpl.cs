b87a130619b1e5406a96ac58ac8ea66f242f5e71
Materal <cloomcmx1554@hotmail.com>
2026-03-02 10:59:49 +0800

    style: 更新代码格式

--- Materal.TTA.SqlServerEFRepository/SqlServerCacheEFRepositoryImpl.cs
+++ Materal.TTA.SqlServerEFRepository/SqlServerCacheEFRepositoryImpl.cs
@@ -2,20 +2,19 @@ using Materal.Utils.Caching;
 using Microsoft.Data.SqlClient;
 using System.Data;
 
-namespace Materal.TTA.SqlServerEFRepository
+namespace Materal.TTA.SqlServerEFRepository;
+
+/// <summary>
+/// SqlServer缓存EF仓储
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <typeparam name="TPrimaryKeyType"></typeparam>
+/// <typeparam name="TDBContext"></typeparam>
+public abstract class SqlServerCacheEFRepositoryImpl<T, TPrimaryKeyType, TDBContext>(TDBContext dbContext, ICacheHelper cacheManager) : CacheEFRepositoryImpl<T, TPrimaryKeyType, TDBContext>(dbContext, cacheManager)
+    where T : class, IEntity<TPrimaryKeyType>, new()
+    where TPrimaryKeyType : struct
+    where TDBContext : DbContext
 {
-    /// <summary>
-    /// SqlServer缓存EF仓储
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <typeparam name="TPrimaryKeyType"></typeparam>
-    /// <typeparam name="TDBContext"></typeparam>
-    public abstract class SqlServerCacheEFRepositoryImpl<T, TPrimaryKeyType, TDBContext>(TDBContext dbContext, ICacheHelper cacheManager) : CacheEFRepositoryImpl<T, TPrimaryKeyType, TDBContext>(dbContext, cacheManager)
-        where T : class, IEntity<TPrimaryKeyType>, new()
-        where TPrimaryKeyType : struct
-        where TDBContext : DbContext
-    {
-        /// <inheritdoc/>
-        protected override IDbConnection GetConnection(string connectionString) => new SqlConnection(connectionString);
-    }
+    /// <inheritdoc/>
+    protected override IDbConnection GetConnection(string connectionString) => new SqlConnection(connectionString);
 }
